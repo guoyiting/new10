1cce172e321dd382251c087abcd76321482b94f1
guoyiting <43691856+guoyiting@users.noreply.github.com>
2019-04-16 11:21:23 +0800

    div

---
+++ div/index.js
@@ -0,0 +1,9 @@
+
+  function init() {
+    var divBiaoQian = document.getElementById('agou')
+    divBiaoQian.innerHTML=''
+    divBiaoQian.setAttribute('style','height:200px;width:280px;background-color:pink;margin-left:400px;margin-top:400px')
+  }
+  window.onload = init;
+
+
